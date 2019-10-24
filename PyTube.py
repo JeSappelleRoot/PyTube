390b7eaa65aadf683d2ef2b463420fbc428a867c
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-10-24 18:13:57 -0500

    Function to download single file from URL is OK

--- PyTube.py
+++ PyTube.py
@@ -1,6 +1,7 @@
 import youtube_dl
 import pydub
-
+import argparse
+from termcolor import colored
 
 def displayBanner():
 
@@ -20,25 +21,24 @@ def displayBanner():
 
 
 
-
-
-
 # --------------------------------------------------------------------------------
 
-def downloadSingleMusic(path, url, outFormat):
+def downloadMusic(path, url, outFormat, name, q, v):
+# Function to download a single audio file from Youtube URL
 
+    # Define a downloader
     yDownloader = youtube_dl.YoutubeDL()
 
+    # Define options for youtube downloader
+    singleURLOptions = {
 
-    yDownloaderOptions = {
-
-            'verbose': False,
-            'quiet': False,
+            'verbose': v,
+            'quiet': q,
             'fixup': 'detect_or_warn',                      # Automatically correct known faults of the file.
             'format': 'bestaudio/best',                     # choice of quality
             'extractaudio' : True,                          # only keep the audio
             'outtmpl': path,                                # name the file the title of the video
-            'noplaylist' : True,                            # only download single song, not playlist
+            'noplaylist' : False,                            # only download single song, not playlist
 
             'postprocessors': [{                            # Define postprocessors options
                 'key': 'FFmpegExtractAudio',                # Which engine to perform postprocessors actions                
@@ -47,15 +47,20 @@ def downloadSingleMusic(path, url, outFormat):
                 }],
         }
 
+    print(colored(f"[+] Downloading music from {name}",'yellow'))
+    # Try/Except to avoid downloading error
     try:
-
-        with youtube_dl.YoutubeDL(yDownloaderOptions) as yDownloader:
+        # With statement to download 
+        with youtube_dl.YoutubeDL(singleURLOptions) as yDownloader:
             yDownloader.download([url])
 
     except youtube_dl.DownloadError as e:
-        print("[!] An error occured during downloading video : ")
+        print(colored("[!] An error occured during downloading video : ",'red'))
         print(e)
         exit()
+
+    print(colored("[+] Downloaded successfully\n",'green'))
+
 
     return
 
@@ -65,7 +70,11 @@ def getInfo(url):
 # Function to get info about a video, given by URL
 
     # Define options to set quiet mode
-    getInfoOptions = {'quiet': True, 'verbose':False}
+    getInfoOptions = {
+                        'quiet': True,
+                        'verbose':False,
+                        'fixup': 'detect_or_warn',                        
+                        }
 
     # Initilize Youtube Downloader
     yDownloader = youtube_dl.YoutubeDL(getInfoOptions)
@@ -76,15 +85,18 @@ def getInfo(url):
     try:
         # Get info
         yMetaData = yDownloader.extract_info(url, download=False)
+
+
     except youtube_dl.DownloadError as e:
         # Catch errors ?
-        print("[!] An error occured during get video info : ")
+        print(colored("[!] An error occured during get video info : ",'red'))
         print(e)
         exit()
 
     # Append info to list
     infoList.append(yMetaData['id'])
     infoList.append(yMetaData['title'])
+
 
     return infoList
 
@@ -94,22 +106,6 @@ def getInfo(url):
 # ------------------------------------------------------------------
 
 
-displayBanner()
-
-url = r'https://www.youtube.com/watch?v=fEqrt6nZTS4'
-
-
-info = getInfo(url)
-
-outputFolder = r'/home/scratch/Downloads'
-tempName = f"Temp_{info[0]}.webm"
-musicFullPath = f"{outputFolder}/{tempName}"
-
-# Can be mp3/flac/aac/wav
-outFormat = 'wav'
-
-downloadSingleMusic(musicFullPath,url,outFormat)
-
 # Sources
 
 # https://willdrevo.com/downloading-youtube-and-soundcloud-audio-with-python-and-pandas
@@ -118,10 +114,35 @@ downloadSingleMusic(musicFullPath,url,outFormat)
 # https://spapas.github.io/2018/03/06/easy-youtube-mp3-downloading/
 # https://github.com/ytdl-org/youtube-dl/issues/10328
 
-
-
 # https://pythonbasics.org/convert-mp3-to-wav/
 
-
-# Postprocessors ? 
+# Postprocessors options 
 # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/postprocessor/ffmpeg.py
+
+
+
+displayBanner()
+
+# Single music url
+singleURL = r'https://www.youtube.com/watch?v=fEqrt6nZTS4'
+
+# Playlis url
+playlistURL = r'https://www.youtube.com/watch?v=IVJ3aRQso9k'
+
+info = getInfo(singleURL)
+
+outputFolder = r'/home/scratch/Downloads'
+videoName = info[1]
+tempName = f"{videoName}.webm"
+musicFullPath = f"{outputFolder}/{tempName}"
+
+q = True
+v = False
+
+# Can be mp3/flac/aac/wav
+outFormat = 'mp3'
+
+downloadMusic(musicFullPath, singleURL, outFormat, videoName, q, v)
+
+
+#getInfo(playlistURL)
