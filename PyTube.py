4a23b4bbd927ff86a9541181dd3dab9877e34228
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-10-24 21:57:16 -0500

    Feat single music with URL/ID (with error management), add verbosity count)

--- PyTube.py
+++ PyTube.py
@@ -1,7 +1,8 @@
-from os import path
+import sys
 import pydub
 import argparse
 import youtube_dl
+from os import path
 from termcolor import colored
 
 def displayBanner():
@@ -105,40 +106,43 @@ def getInfo(url):
 # ------------------------------ Main ------------------------------
 # ------------------------------------------------------------------
 
+# Define an argument parser
 parser = argparse.ArgumentParser(
-
 formatter_class=argparse.RawDescriptionHelpFormatter,
-
-description="""
-# --------------------------------------------------------- #
-' PyTube is designed to download easily music from Youtube. '
-' From URL, mutli URL in a text file or a playlist.         '
-' Many audio format can be used (mp3, aac, flac, wav)       '
-# --------------------------------------------------------- #
-"""
 )
 
-parser.add_argument('--mode', help='Specifiy the mode to use [url/playlist/file]', choices=['url','playlist','file'], required=True)
-parser.add_argument('--folder', help='Specify the folder where music will be saved', required=True)
-parser.add_argument('--name', help='Specify the name of the audio file, else PyTube will used the video name instead')
+# Add a exclusive group to make conflicts between args
+target = parser.add_mutually_exclusive_group()
+
+parser.add_argument('--mode', help='Specifiy the mode to use [url/playlist/file]', choices=['url','playlist','file'])
+target.add_argument('--url', help='Get music from this url, only for URL and playlist mode')
+target.add_argument('--id', help='Get music from this Youtube video ID (only for URL and playlist mode)')
+target.add_argument('--file', help='File which contain URL')
+parser.add_argument('--output', help='Specify the folder where music will be saved', required=True)
+parser.add_argument('--name', help='Specify the name (without extension) of the audio file, else PyTube will used the video name instead')
 parser.add_argument('--format', help='Specify format of the audio file [mp3/acc/flac/wav] (Default is mp3)', default='mp3',choices=['mp3','aac','flac','wav'])
-parser.add_argument('--quiet', help='Display some informations during downloading like progression bar, video\'s ID (True by default)', default=True, action='store_true')
-parser.add_argument('--verbose', help='Increase verbosity to debug video downloading (False by default)', default=False, action='store_true')
+parser.add_argument('-v', help='Increase verbosity to debug video downloading (repeat -v to increase verbosity)', default=0, action='count')
 
 displayBanner()
 
 
+# Parse arguments
+args = parser.parse_args()      
 
+mode = args.mode                            # Parse mode
+if mode == 'url' or mode == 'playlist':     # If mode a single url
+    if args.url:                            # If full URL is specified
+        target = args.url                   # target will be the URL
+    elif args.id:                           # else if ID is specified
+        target = args.id                    # target will be the video ID
+    else:                                   # else target variable is None
+        target = None
 
-args = parser.parse_args()      # Parse arguments
-mode = args.mode                # Parse mode
-outputFolder = args.folder      # Parse output folder
+outputFolder = args.output      # Parse output folder
 outFormat = args.format         # Parse output audio file format
-q = args.quiet                  # Parse quiet argument (True by default)
-v = args.verbose                # Parse verbose argument (False by default)
+verbose = args.v                # Parse verbose argument (0 by default)
 if args.name:                   # Parse name if specified
     videoName = args.name
-
 
 # If less than 2 arguments
 # Print help section and quit
@@ -147,13 +151,10 @@ if len(sys.argv) < 2:
     exit()
 
 # Test if output folder exist
-if not path.isdir(outFolder):
+if not path.isdir(outputFolder):
     print(colored("Output folder does'nt exist",'red'))
     print(colored(outputFolder,'red'))
     exit()
-
-
-
 
 
 
@@ -177,18 +178,21 @@ singleURL = r'https://www.youtube.com/watch?v=fEqrt6nZTS4'
 # Playlis url
 playlistURL = r'https://www.youtube.com/watch?v=IVJ3aRQso9k'
 
-info = getInfo(singleURL)
 
-outputFolder = r'/home/scratch/Downloads'
-videoName = info[1]
+if 'videoName' not in locals(): 
+    info = getInfo(singleURL)
+    videoName = info[1]
+
 tempName = f"{videoName}.webm"
 musicFullPath = f"{outputFolder}/{tempName}"
 
+print(target)
 
-# Can be mp3/flac/aac/wav
-outFormat = 'mp3'
-
-#downloadMusic(musicFullPath, singleURL, outFormat, videoName, q, v)
+if mode == 'url' and target:
+    downloadMusic(musicFullPath, target, outFormat, videoName, q, v)
+else:
+    print(colored('[!] A URL or and ID must be specified in url mode','red'))
+    exit()
 
 
 #getInfo(playlistURL)
